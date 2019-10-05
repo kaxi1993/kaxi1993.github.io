d8afe4b3f8740a35a4cde7629592f46adce82b72
kaxi1993 <kakhidze2012@gmail.com>
2019-10-05 14:39:03 +0400

    React app initial commit

---
+++ build/precache-manifest.4bdb02d3a7d2a0d7f7c2491d494920b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f765e530b67d6c2b690152376c909e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b531bffde93fb617ce",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "206bfe8f87fa8cfcc164",
+    "url": "/static/js/2.bfb01147.chunk.js"
+  },
+  {
+    "revision": "84b531bffde93fb617ce",
+    "url": "/static/js/main.656983fb.chunk.js"
+  },
+  {
+    "revision": "49900917215b16a7eb0d",
+    "url": "/static/js/runtime~main.60a0bfdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
