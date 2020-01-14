5d082ca3dcd96b5f8ed20036752feb4e622f6d73
kaxi1993 <kakhidze2012@gmail.com>
2020-01-15 03:36:21 +0400

    Update experience section

--- js/resume.js
+++ js/resume.js
@@ -4,31 +4,31 @@
         years: 'Jun 2017 - Nov 2019',
         location: 'Georgia',
         company: 'Pulsar AI',
-        description: 'Don’t ever play yourself. In life there will be road blocks but we will over come it. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. You do know, you do know that they don’t want you to have lunch.'
+        description: 'Developed and shipped 3 successfull messenger chatbots. Created fully automated system to deploy code from bitbucket to AWS. Developed content management system for chatbots. Worked with a variety of different languages, frameworks such as JavaScript, React, Angular, Node, Python, Ruby etc.'
     }, {
         title: 'Web Developer',
         years: 'Jul 2016 - Jun 2017',
         location: 'United States',
         company: 'Usario',
-        description: 'Don’t ever play yourself. In life there will be road blocks but we will over come it. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. You do know, you do know that they don’t want you to have lunch.'
+        description: 'Developed survey creator website. I was a fullstack developer, used Node Strongloop framework on backend and Angular on frontend.'
     }, {
         title: 'Web Developer',
         years: 'Feb 2017 - Jun 2017',
         location: 'Australia',
         company: 'FirstMac',
-        description: 'Don’t ever play yourself. In life there will be road blocks but we will over come it. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. You do know, you do know that they don’t want you to have lunch.'
+        description: 'I rewrote the REST API to Node, added unit tests. Used mocha for testing.'
     }, {
         title: 'Web Developer',
         years: 'Jan 2015 - Jul 2016',
         location: 'United States',
         company: 'KVSocial',
-        description: 'Don’t ever play yourself. In life there will be road blocks but we will over come it. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. You do know, you do know that they don’t want you to have lunch.'
+        description: 'Worked on two websites: the first for creating banners, and the second for online presentations. I was a frontend developer and resolved very complex problems using createjs and fabricjs.'
     }, {
         title: 'Junior Web Developer',
         years: 'Dec 2013 - Jan 2015',
         location: 'Australia',
         company: 'Aurazenix',
-        description: 'Don’t ever play yourself. In life there will be road blocks but we will over come it. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. You do know, you do know that they don’t want you to have lunch.'
+        description: 'My responsibility was to convert psd to html and to add animations and some functionality using css and jQuery.'
     }]
 
     var years = d.getElementById('years')
